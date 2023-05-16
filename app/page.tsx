8aed4422dfd2c8238e687539acdca785647298ad
MrGoropeza <goropeza8@gmail.com>
2023-05-16 10:59:06 -0300

    feat: responsive titles on home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,13 @@ import { Routes } from "./models";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center flex-col h-full gap-4">
-      <h1 className="text-6xl">Welcome to Rick & Morty app</h1>
-      <h2 className="text-4xl">What do you want to see?</h2>
+    <div className="flex h-full flex-col items-center justify-center gap-4">
+      <h1 className="text-center text-4xl md:text-6xl">
+        Welcome to Rick & Morty App
+      </h1>
+      <h2 className="text-center text-2xl md:text-4xl">
+        What do you want to see?
+      </h2>
       <Navigator
         pathNames={[Routes.CHARACTERS, Routes.LOCATIONS, Routes.EPISODES]}
       />
