c507c4d0c6f2fb1ad6d3ea56b21165fa4d5af8fb
MrGoropeza <goropeza8@gmail.com>
2023-05-17 00:49:32 -0300

    feat: removed information route

--- app/information/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from "react";
-
-const CharactersLayout = ({ children }: PropsWithChildren) => {
-  return <div className="flex flex-wrap justify-center gap-4">{children}</div>;
-};
-
-export default CharactersLayout;
--- app/models/routes.model.ts
+++ app/models/routes.model.ts
@@ -1,8 +1,8 @@
 export const Routes = {
   HOME: { path: "/", name: "Home" },
-  CHARACTERS: { path: "information/characters", name: "Characters" },
-  LOCATIONS: { path: "information/locations", name: "Locations" },
-  EPISODES: { path: "information/episodes", name: "Episodes" },
+  CHARACTERS: { path: "characters", name: "Characters" },
+  LOCATIONS: { path: "locations", name: "Locations" },
+  EPISODES: { path: "episodes", name: "Episodes" },
 };
 
 export interface Route {
