2ad17e22c9927daab61dfb442815a7048aad4097
MrGoropeza <goropeza8@gmail.com>
2023-05-26 02:56:46 -0300

    feat: character page with pagination & filters

--- app/characters/page.tsx
+++ app/characters/page.tsx
@@ -1,11 +1,16 @@
+"use client";
+
 import { Routes } from "@/app/models";
 import { Navigator } from "@/components";
+import { Paginator, PaginatorPageChangeEvent } from "primereact/paginator";
+import { useEffect, useState } from "react";
 import { episodeService } from "../episodes/services";
-import { CharacterCard } from "./components";
+import { CharacterCard, CharacterFilterComponent } from "./components";
+import { Character, CharacterFilter } from "./models";
 import { characterService } from "./services";
 
-async function fetchCharacters() {
-  const response = await characterService.getAll(1);
+async function fetchCharacters(page: number, filter?: CharacterFilter) {
+  const response = await characterService.getAll(page, filter);
 
   const firstEpisodesIds = response.results.map((character) => {
     return episodeService.getId(character.episode[0]);
@@ -18,21 +23,55 @@ async function fetchCharacters() {
     character.episodes.push(firstEpisodes[index]);
   });
 
-  return response.results;
+  return response;
 }
 
-const Characters = async () => {
-  const characters = await fetchCharacters();
+const Characters = () => {
+  const [characters, setCharacters] = useState<Character[]>([]);
+  const [page, setPage] = useState<number>(0);
+  const [totalRecords, setTotalRecords] = useState<number>(0);
+  const [filter, setFilter] = useState<CharacterFilter>({});
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetchCharacters(page + 1, filter)
+      .then((data) => {
+        setLoading(false);
+        setCharacters(data.results);
+        setTotalRecords(data.info.count);
+      })
+      .catch(() => {
+        // setCharacters([]);
+        // setTotalRecords(0);
+      });
+  }, [page, filter]);
+
+  const onPageChange = (event: PaginatorPageChangeEvent) => {
+    setPage(event.first / 20);
+  };
 
   return (
-    <>
+    <main className="flex max-h-full flex-col">
       <Navigator pathNames={[Routes.HOME, Routes.LOCATIONS]} />
-      <div className="m-4 grid grid-cols-1 gap-2 lg:grid-cols-2 2xl:grid-cols-3">
+
+      <CharacterFilterComponent loading={loading} setFilter={setFilter} />
+
+      <div className="m-4 grid grid-cols-1 gap-2 overflow-y-auto lg:grid-cols-2 2xl:grid-cols-3">
         {characters.map((character) => (
           <CharacterCard key={character.id} data={character} />
         ))}
       </div>
-    </>
+
+      <Paginator
+        className="bg-slate-800/70"
+        pageLinkSize={3}
+        first={page * 20}
+        rows={20}
+        totalRecords={totalRecords}
+        onPageChange={onPageChange}
+      />
+    </main>
   );
 };
 
